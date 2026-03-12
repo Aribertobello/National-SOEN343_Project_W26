df2466aa5c58ac87ddcfef5809044b3ff18334a2
Curtis Moxebo <cmoxebo@gmail.com>
2026-03-12 18:46:11 -0400

    Login page

--- frontend/src/pages/authentication/AuthLayout.tsx
+++ frontend/src/pages/authentication/AuthLayout.tsx
@@ -2,8 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
     return (
-        <div>
-            Layout
+        <div className="flex min-h-screen items-center justify-center bg-background">
             <Outlet />
         </div>
     );
--- frontend/src/pages/authentication/LoginPage.tsx
+++ frontend/src/pages/authentication/LoginPage.tsx
@@ -1,8 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
     return (
-        <div>
-            Login page
-        </div>
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                    Enter your email below to login to your account.
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="m@example.com" required autoComplete="off" />
+                </div>
+                <div className="grid gap-2">
+                    <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                        <Link to="/forgot-password" className="ml-auto inline-block text-sm underline">
+                            Forgot your password?
+                        </Link>
+                    </div>
+                    <Input id="password" type="password" required autoComplete="off" />
+                </div>
+            </CardContent>
+            <CardFooter className="flex flex-col gap-4">
+                <Button className="w-full">Sign in</Button>
+                <div className="text-center text-sm">
+                    Don't have an account?{" "}
+                    <Link to="/register" className="underline">
+                        Sign up
+                    </Link>
+                </div>
+            </CardFooter>
+        </Card>
     );
 };
 
